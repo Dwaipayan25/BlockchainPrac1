a7f9e7e3cfb8175a1f96df13d8dde2f0e0022bed
Dwaipayan25 <dwaipayan.dehury@gmail.com>
2023-01-01 13:35:51 +0530

    connect account

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import abi from './contracts/blc.json';
+import {useState,useEffect} from 'react';
+import {ethers} from 'ethers'; 
+
 
 function App() {
+
+  const [state,setState] = useState({
+    provider:null,
+    signer:null,
+    contract:null,
+  })
+
+  const [account,setAccount] = useState('None');
+  useEffect(()=>{
+    const connectWallet=async()=>{
+      const contractAddress = "0x0D4B291E519Db3a65a1351886E106E126bFEdc25";
+      const contractABI=abi.abi;
+      try{
+        const {ethereum}=window;
+        if(ethereum){
+          const accounts=await ethereum.request({method:"eth_requestAccounts"});
+
+          window.ethereum.on("chainChanged",()=>{
+            window.location.reload();
+          })
+
+          window.ethereum.on("accountsChanged",()=>{
+            window.location.reload();
+          })
+        
+          const provider=new ethers.providers.Web3Provider(window.ethereum);
+          const signer=provider.getSigner();
+          const contract=new ethers.Contract(contractAddress,contractABI,signer);
+          console.log(accounts[0]);
+          setAccount(accounts[0]);
+          setState({provider,signer,contract});
+        }else{
+          alert("please install metamask")
+        }
+      }
+      catch{
+        console.log("error");
+      }
+    };
+    connectWallet();
+  },[])
+  console.log(state);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Connected Account: {account}
     </div>
   );
 }
